9ed5bcd44c5ab6f510ad7c97bc1a1e992fe47cc7
Natalia Antonova <5555482@gmail.com>
2015-09-10 21:57:25 +0100

    Create categories and cart

--- server/startup.js
+++ server/startup.js
@@ -1,16 +1,29 @@
 Meteor.startup(function(){
     if(Products.find().count() === 0){
-      Products.insert({thumb:'applepie.jpeg',name:'Apple Pie',desc:'Decadent Apple Pie',price:2.50,catName:'Fruity'});
-      Products.insert({thumb:'blackberrysmoothie.jpeg',name:'Blackberry Smoothie',desc:'Luscious Blackbery Smoothie',price:2.50,catName:'Fruity'});
-      Products.insert({thumb:'mod1.jpg',name:'EGO Twist',desc:'Ego Twist Mod',price:23.50,catName:'Mods'});
+      Products.insert({thumb:'almondtoeshoes.jpeg',name:'Almond Toe Court Shoes,Patent Black',desc:'Almond Toe Court Shoes,Patent Black',price:99.00,catName:'women’s footwear'});
+      Products.insert({thumb:'suedeshoes.jpeg',name:'Suede Shoes, Blue',desc:'Suede Shoes, Blue',price:42.00,catName:'women’s footwear'});
+      Products.insert({thumb:'loafers.jpeg',name:'Leather Driver Saddle Loafers,Tan',desc:'Leather Driver Saddle Loafers,Tan',price:34.00,catName:'men’s footwear'});
+      Products.insert({thumb:'flipflopr.jpeg',name:'Flip Flops, Red',desc:'Flip Flops, Red',price:19.00,catName:'men’s footwear'});
+      Products.insert({thumb:'flipflopb.jpeg',name:'Flip Flops, Blue',desc:'Flip Flops, Blue',price:19.00,catName:'men’s footwear'});
+      Products.insert({thumb:'shorts.jpeg',name:'Cotton Shorts, Medium Red',desc:'Cotton Shorts,Medium Red',price:30.00,catName:'women’s casualwear'});
+      Products.insert({thumb:'shirt.jpeg',name:'Fine Stripe Short Sleeve Shirt,Grey',desc:'Fine Stripe Short Sleeve Shirt,Grey',price:49.99,catName:'men’s casualwear'});
+      Products.insert({thumb:'shirtg.jpeg',name:'Fine Stripe Short Sleeve Shirt,Green',desc:'Fine Stripe Short Sleeve Shirt,Green',price:39.99,catName:'men’s casualwear'});
+      Products.insert({thumb:'waistcoat.jpeg',name:'Fine Stripe Short Sleeve Shirt,Grey',desc:'Fine Stripe Short Sleeve Shirt,Grey',price:49.99,catName:'men’s casualwear'});
+      Products.insert({thumb:'blazer.jpeg',name:'Lightweight Patch Pocket Blazer,Deer',desc:'Lightweight Patch Pocket Blazer,Deer',price:175.00,catName:'men’s formalwear'});
+      Products.insert({thumb:'dress.jpeg',name:'Bird Print Dress,Black',desc:'Bird Print Dress,Black',price:270.00,catName:'women’s formalwear'});
+      Products.insert({thumb:'twistdress.jpeg',name:'Mid Twist Cut Out Dress,Pink',desc:'Mid Twist Cut Out Dress,Pink',price:540.00,catName:'women’s formalwear'});
+      
 
     }
     if(Categories.find().count() === 0){
-        var hwid = Categories.insert({name:'HARDWARE'});
-        var juid = Categories.insert({name:'JUICE'});
-        SubCategories.insert({name:'Mods',cat:hwid});
-        SubCategories.insert({name:'Fruity',cat:juid});
-        SubCategories.insert({name:'Sweet',cat:juid});
+        var wid = Categories.insert({name:'WOMEN'});
+        var mid = Categories.insert({name:'MEN'});
+        SubCategories.insert({name:'women’s footwear',cat:wid});
+        SubCategories.insert({name:'women’s casualwear',cat:wid});
+        SubCategories.insert({name:'women’s formalwear',cat:wid});
+        SubCategories.insert({name:'men’s footwear',cat:mid});
+        SubCategories.insert({name:'men’s casualwear',cat:mid});
+        SubCategories.insert({name:'men’s formalwear',cat:mid});
     }
 
 });
